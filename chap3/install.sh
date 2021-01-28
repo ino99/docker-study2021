7d48b77016a0cb6cbb1fbc5d3b2923d761d89ab0
root <root@localhost.localdomain>
2021-01-28 19:19:28 +0900

    docker start 記述追加

--- chap3/install.sh
+++ chap3/install.sh
@@ -9,6 +9,7 @@ sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo  
 sudo yum install -y docker-ce docker-ce-cli containerd.io  
 sudo yum install -y conntrack  
+sudo systemctl start docker  
 sudo systemctl enable docker  
 
 #Install kubectl
